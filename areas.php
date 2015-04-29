e872c7f36afe777da2e14525d9b4dcd0c17dc9c0
takumi314 <isospin_symmetry@me.com>
2015-04-29 16:23:16 +0800

    データベース名に謝り発見→修正済。

--- areas.php
+++ areas.php
@@ -12,7 +12,7 @@ if (!$link) {
 }
 
 	//データーベースを選択する。
-	mysql_select_db('friends_sysmtem', $link);   // 
+	mysql_select_db('friends_system', $link);   // 
 
 	//都道府県データを取得するために、SQlを作成して結果を取得
 	$sql = "SELECT * FROM `friends_system`.`todo-huken_list` ORDER BY `todo-huken_id` ASC" ;
@@ -38,28 +38,27 @@ if (!$link) {
     <select name="from_id" onchange="dropsort()">
         <option value=0> --- 選択してください --- </option>
  <ul> -->
-<?php   //データベースから取得した都道府県を順に表示する。
 
+ 		<table>
+<?php   //データベースから取得した都道府県を順に表示する。
 	if ($result !== false && mysql_num_rows($result)){
 			while ($post = mysql_fetch_assoc($result)){
 
-				//$sql = "SELECT count(*) FROM `friends_system`.`friends_list` LEFT OUTER JOIN `todo-huken_list` ";
-   				//sql .= "ON `friends_list`.`from_id` = `todo-huken_list`.`todo-huken_id` "; 
-   				//$sql .= "WHERE `todo-huken_list`.`todo-huken_id` =".htmlspecialchars($post['todo-huken_id']).";";	
-				$sql = "SELECT * FROM `friends_list` ";
+				$sql = "SELECT count(*) FROM `friends_system`.`friends_list` LEFT OUTER JOIN `friends_system`.`todo-huken_list` ";
+   				$sql .= "ON `friends_system`.`friends_list`.`from_id` = `friends_system`.`todo-huken_list`.`todo-huken_id` "; 
+   				$sql .= "WHERE `friends_system`.`todo-huken_list`.`todo-huken_id` =".htmlspecialchars($post['todo-huken_id']).";";	
+				//$sql_2 = "SELECT * FROM `friends_list` ";
 
-   				var_dump($sql);
+   				//var_dump($sql);
    				$population_area = mysql_query($sql,$link);
    				$num = mysql_fetch_assoc($population_area);
-
-   				var_dump($num);
-   				var_dump($population_area);
+   				
    					//人数が１人以上のときにリンクを有効とする
-   					if ($population_area == 0) {
-   						echo '<li>'. htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'). '（'.$population_area.'）</li>' ;
+   					if ($num['count(*)'] == 0) {
+   						echo '<td>'. htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'). '（'.$num['count(*)'].'）</td><br/>' ;
    					} else{
 	 	     			//echo '<option value="' .htmlspecialchars($post['todo-huken_id'], ENT_QUOTES, 'UTF-8').'" >' .htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'). '</option>';
-	 	     			echo '<li><a href="./friends.php?from_id=' .htmlspecialchars($post['todo-huken_id'], ENT_QUOTES, 'UTF-8'). ' "> '. htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'). '（'.$population_area.'）</a></li>' ;
+	 	     			echo '<td><a href="./friends.php?from_id=' .htmlspecialchars($post['todo-huken_id'], ENT_QUOTES, 'UTF-8'). ' "> '. htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'). '（'.$num['count(*)'].'）</a></td><br/>' ;
 					}
 
 				//mysql_free_result($population_area);
@@ -71,15 +70,15 @@ if (!$link) {
 
 	$result = mysql_query($sql, $link);
 ?>
-
+		</table>
 <!-- </ul> 
 		</select>
     <input type="submit" value="検索" size=120 />
 </form>
 -->
 
-	<h3><a href="http://192.168.33.10/new_friend.php"><font>おともだちの登録</font></a></h3>
-	<h3><a href="http://192.168.33.10/index.html"><font>Top</font></a></h3>
+	<h3><a href="http://192.168.33.20/new_friend.php"><font>おともだちの登録</font></a></h3>  <!--$_SERVER['   ']に書き換える。-->
+	<h3><a href="http://192.168.33.20/index.html"><font>Top</font></a></h3>
 
 </body>
 </html>
